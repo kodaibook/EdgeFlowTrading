8458d6d84903f0668fe80c15e3099d91d6254ef1
kodaibook <valentin87.pop@gmail.com>
2025-09-20 21:20:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('EdgeFlow site loaded');
+<script>
+// Meniu mobil
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.querySelector('.nav__links');
+if (navToggle) navToggle.addEventListener('click', () => navLinks.classList.toggle('open'));
+
+// Tilt subtil pe mouse
+document.addEventListener('mousemove', (e) => {
+  const x = (e.clientX / innerWidth - 0.5) * 2;
+  const y = (e.clientY / innerHeight - 0.5) * 2;
+  document.querySelectorAll('.tilt').forEach(el => {
+    el.style.transform = `rotateX(${y * 6}deg) rotateY(${x * -8}deg) translateZ(10px)`;
+  });
+});
+</script>
